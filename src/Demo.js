13008e101072c75adac3739f67e95d1475ec513f
unknown <dev.dounets.nmtuan@gmail.com>
2019-05-08 00:38:52 -0700

    update learn react

---
+++ src/Demo.js
@@ -0,0 +1,15 @@
+import React ,{ Component } from 'react';
+
+
+
+class Demo extends Component {
+  render(){
+    return (
+      <div className="App">
+       <h1>Hello ReactJS</h1>
+       <p>chao mung ban den voi reactjs</p>
+      </div>
+    );
+  }
+}
+export default Demo;
